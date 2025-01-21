d37c2b506529fa4b55e2fc091b760a234e00ca65
Keloran <keloran@develbox.info>
2025-01-21 11:50:24 +0000

    fix(server): simplify context creation and caller usage
    Refactor `createContext function to remove unnecessary async 
    handling, improving performance. Update the `caller` initialization 
    to eliminate a TypeScript error related to `HydrateClient`, ensuring 
    compatibility with the `createHydrationHelpers` function.

--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -12,8 +12,8 @@ import { createQueryClient } from "./query-client";
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a tRPC call from a React Server Component.
  */
-const createContext = cache(async () => {
-  const heads = new Headers(await headers());
+const createContext = cache(() => {
+  const heads = new Headers(headers());
   heads.set("x-trpc-source", "rsc");
 
   return createTRPCContext({
@@ -22,9 +22,7 @@ const createContext = cache(async () => {
 });
 
 const getQueryClient = cache(createQueryClient);
-const caller = createCaller(createContext);
+const caller = createCaller(createContext)
 
-export const { trpc: api, HydrateClient } = createHydrationHelpers<AppRouter>(
-  caller,
-  getQueryClient
-);
+// @ts-expect-error - caller complains about HydrateClient
+export const { trpc: api, HydrateClient } = createHydrationHelpers<AppRouter>(caller, getQueryClient);
